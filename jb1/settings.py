94a100a26b2836be40e3446933741a75c18ff892
JosephBarasa <josephbarasa622@gmail.com>
2024-07-26 11:23:25 +0300

    sss

--- jb1/settings.py
+++ jb1/settings.py
@@ -58,7 +58,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'JB1',
+    'jb1',
+    'Home',
+    'Accounts'
 ]
 
 MIDDLEWARE = [
