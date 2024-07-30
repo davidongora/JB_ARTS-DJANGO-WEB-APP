2e47aac3ea6794f4ee7e3cf1cfda8e57cff3570f
JosephBarasa <josephbarasa622@gmail.com>
2024-07-30 13:24:16 +0300

    Deployment

--- jb1/urls.py
+++ jb1/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+admin.site.site_header="JB ARTS"
+admin.site.site_title="JB ARTS"
+admin.site.index_title="JB ARTS"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("Home.urls")),
